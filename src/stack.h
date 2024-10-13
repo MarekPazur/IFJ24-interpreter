a0fbe1f896ef6d34f7b674929d6b312711760efb
RobertGlos <121170882+RobertGlos@users.noreply.github.com>
2024-10-13 20:12:28 +0200

    Create stack.h

---
+++ src/stack.h
@@ -0,0 +1,25 @@
+#ifndef STACK_H // Resolves issues regarding inclusion in multiple source files
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "token.h"
+
+/* ITEM STRUCTURE */
+struct item {
+    token_t token;
+    struct item *next;
+};
+
+/* STACK STRUCTURE */
+struct stack {
+    struct item *last;
+};
+
+/* FUNCTION PROTOTYPES */
+struct stack* create_stack();
+void push(struct stack *s, token_t token);
+token_t pop(struct stack *s);
+void free_stack(struct stack *s);
+
+#endif
