3cb91ffeeaeba7ec4ce3c8cfb07288aa25932fe6
RobertGlos <121170882+RobertGlos@users.noreply.github.com>
2024-10-17 12:15:56 +0200

    Update stack.c
    added debugging for stacks

--- src/stack.c
+++ src/stack.c
@@ -1,5 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "stack.h"
 #include "token.h"
+
+/* DEBUG STATE */
+static bool debug_mode = false; // Using boolean for more clean looking debug function
+
+/* STACK DEBUG TOGGLE */
+void s_debug() {
+    debug_mode = !debug_mode; // Toggle the debug mode
+}
+
+/* Current stack output */
+void print_stack_state(struct stack *s) {
+    printf("Current stack state: ");
+    struct item *current = s->last;
+    while (current) {
+        printf("%d ", current->token.id); // Print the integer value of the token ID, I couldn't find a way to display it simply as the enum names outside of using a switch
+        current = current->next;
+    }
+    printf("\n");
+}
+
 /* STACK INIT FUNCTION */
 struct stack* init_stack() {
     return (struct stack *)calloc(1, sizeof(struct stack));
@@ -21,18 +44,30 @@ void push(struct stack *s, token_t token) {
     if (new_item) {
         new_item->next = s->last;
         s->last = new_item;
+
+        if (debug_mode) {
+            printf("Pushed token ID: %d\n", token.id); // Print the integer value of the pushed token ID
+            print_stack_state(s);
+        }
     }
 }
 
 /* STACK POP FUNCTION */
 token_t pop(struct stack *s) {
-    if (!s->last) { //Error if stack has no items, probably won't be an issue
+    if (!s->last) { // Error if stack has no items
         exit(1);
     }
     struct item *top_item = s->last;
     token_t token = top_item->token;
     s->last = top_item->next;
     free(top_item);  // Free item from memory
+
+    // Debug output
+    if (debug_mode) {
+        printf("Popped token ID: %d\n", token.id); // Print the integer value of the popped token ID
+        print_stack_state(s);
+    }
+
     return token;
 }
 
