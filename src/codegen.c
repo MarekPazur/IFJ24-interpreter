07722558f3fa0d8a64983e44582d4cf42cafcb32
Jan Ukropec <xukropj00@stud.fit.vutbr.cz>
2024-11-13 16:28:58 +0100

    Some built in functions, fixes

--- src/codegen.c
+++ src/codegen.c
@@ -12,11 +12,19 @@
 #include "compiler_error.h"
 #include "codegen.h"
 
+// Return value global frame variable
+const TTerm cg_var_retval = {.type = VARIABLE_T, .value.var_name = "input", .frame = GLOBAL};
+
 // IFJ24code frame types
 #define GF "GF@"
 #define LF "LF@"
 #define TF "TF@"
 
+/**
+ * Function for getting strings with memory frames
+ * \param frame Constant value of a memory frame
+ * \return String with a corresponding frame and @ eg. "'frame type'F@"
+ */
 const char* get_frame(Frame frame){
     switch(frame){
         case GLOBAL:
@@ -29,16 +37,56 @@ const char* get_frame(Frame frame){
     return "";
 }
 
-void cg_init(void){
-    printf(".IFJcode24\n");
+void cg_term(TTerm term){
+    char* bool_str;
+    switch(term.type){
+        case VARIABLE_T:
+            printf("%s%s", get_frame(term.frame), term.value.var_name);
+            break;
+        case INTEGER_T:
+            printf("int@%d", term.value.int_val);
+            break;
+        case FLOAT_T:
+            printf("float@%a", term.value.float_val);
+            break;
+        case BOOLEAN_T:
+            bool_str = term.value.bool_val ? "true" : "false";
+            printf("bool@%s", bool_str);
+            break;
+        case STRING_T:
+            printf("string@");
+            int i = 0;
+            while(term.value.string[i] != '\0'){
+                char c = term.value.string[i];
+                if(c < 32 || c == '#' || c == '\\'){ // 000-032: Unprintable characters, 035: '#', 092: '\'
+                    printf("\\%03d", c);
+                }
+                else{
+                    putchar(c);
+                }
+                i++;
+            }
+            break;
+        case NULL_T:
+            printf("nil@nil");
+            break;
+        default:
+            error = ERR_COMPILER_INTERNAL;
+            break;
+    }
 }
 
-void cg_create_var(char* var_name){
-    if(var_name == NULL){
-        print_error(ERR_COMPILER_INTERNAL);
+void cg_init(void){
+    printf(".IFJcode24\n");
+    cg_create_var(cg_var_retval);
+}
+
+void cg_create_var(TTerm var){
+    if(var.type != VARIABLE_T || var.value.var_name == NULL){
+        //set_error(ERR_COMPILER_INTERNAL);
         return;
     }
-    printf("defvar %s%s\n", get_frame(LOCAL), var_name);
+    printf("defvar %s%s\n", get_frame(var.frame), var.value.var_name);
 }
 
 void cg_create_frame(void){
@@ -55,7 +103,7 @@ void cg_pop_frame(void){
 
 void cg_call(char* label){
     if(label == NULL){
-        print_error(ERR_COMPILER_INTERNAL);
+        //set_error(ERR_COMPILER_INTERNAL);
         return;
     }
     printf("call %s\n", label);
@@ -65,69 +113,61 @@ void cg_return(void){
     printf("return\n");
 }
 
+void cg_move(TTerm var, TTerm symb){
+    if(var.type != VARIABLE_T){
+        //set_error(ERR_COMPILER_INTERNAL);
+        return;
+    }
+    printf("move ");
+    cg_term(var);
+    putchar(' ');
+    cg_term(symb);
+    putchar('\n');
+}
+
 // IFJ BUILT-IN
 
-void cg_ifj_readstr(char* var_name){
-    if(var_name == NULL){
-        print_error(ERR_COMPILER_INTERNAL);
-        return;
-    }
-    printf("read %s%s string\n", get_frame(LOCAL), var_name);
+void cg_ifj_readstr(void){
+    printf("read ");
+    cg_term(cg_var_retval);
+    printf(" string\n");
 }
 
-void cg_ifj_readi32(char* var_name){
-    if(var_name == NULL){
-        print_error(ERR_COMPILER_INTERNAL);
-        return;
-    }
-    printf("read %s%s int\n", get_frame(LOCAL), var_name);
+void cg_ifj_readi32(void){
+    printf("read %s%s int\n", get_frame(cg_var_retval.frame), cg_var_retval.value.var_name);
 }
 
-void cg_ifj_readf64(char* var_name){
-    if(var_name == NULL){
-        print_error(ERR_COMPILER_INTERNAL);
-        return;
-    }
-    printf("read %s%s float\n", get_frame(LOCAL), var_name);
+void cg_ifj_readf64(void){
+    printf("read %s%s float\n", get_frame(cg_var_retval.frame), cg_var_retval.value.var_name);
 }
 
 void cg_ifj_write(TTerm term){
-    char* bool_str;
-    switch(term.type){
-        case VARIABLE_T:
-            printf("write %s%s\n", get_frame(LOCAL), term.value.var_name);
-            break;
-        case INTEGER_T:
-            printf("write int@%d\n", term.value.int_val);
-            break;
-        case FLOAT_T:
-            printf("write float@%a\n", term.value.float_val);
-            break;
-        case BOOLEAN_T:
-            bool_str = term.value.bool_val ? "true" : "false";
-            printf("write bool@%s\n", bool_str);
-            break;
-        case STRING_T:
-            printf("write string@");
-            int i = 0;
-            while(term.value.string[i] != '\0'){
-                char c = term.value.string[i];
-                if(c < 32 || c == '#' || c == '\\'){ // 000-032: Unprintable characters, 035: '#', 092: '\'
-                    printf("\\%03d", c);
-                }
-                else{
-                    putchar(c);
-                }
-                i++;
-            }
-            putchar('\n');
-            break;
-        case NULL_T:
-            printf("write string@null\n");
-            break;
-        default:
-            print_error(ERR_COMPILER_INTERNAL);
-            break;
+    printf("write ");
+    cg_term(term);
+    putchar('\n');
+}
+
+void cg_ifj_i2f(TTerm term){
+    if(term.type != INTEGER_T){
+        //set error
+        return;
     }
+    printf("int2float ");
+    cg_term(cg_var_retval);
+    putchar(' ');
+    cg_term(term);
+    putchar('\n');
+}
+
+void cg_ifj_f2i(TTerm term){
+    if(term.type != FLOAT_T){
+        //set error
+        return;
+    }
+    printf("float2int ");
+    cg_term(cg_var_retval);
+    putchar(' ');
+    cg_term(term);
+    putchar('\n');
 }
 
--- src/codegen.h
+++ src/codegen.h
@@ -33,22 +33,61 @@ typedef struct term{
         bool bool_val;
         char *string;
     } value;
+    Frame frame; // In case of VARIABLE_T (otherwise can be left uninitialized)
 } TTerm;
 
-//
+// Constants
+extern const TTerm cg_var_retval;
 
+/**
+ * Sets interpreted language to IFJcode24
+ */
 void cg_init(void);
 
-void cg_create_var(char* var_name);
+/**
+ * Creates variable
+ * \param var
+ */
+void cg_create_var(TTerm var);
 
+/**
+ * Creates temporary memory frame and deletes the current one
+ */
 void cg_create_frame(void);
 
+/**
+ * Pushes temporary memory frame to a local frame stack
+ */
 void cg_push_frame(void);
+
+/**
+ * Pops the active local memory frame and replaces the current temporary frame with it
+ */
+void cg_pop_frame(void);
+
+void cg_move(TTerm var, TTerm symb);
 
 // IFJ BUILT-IN
 
-void cg_ifj_readstr(char* var_name);
+/**
+ * Reads a string from stdin and saves it into a variable GF@input
+ */
+void cg_ifj_readstr(void);
 
+/**
+ * Reads an integer from stdin and saves it into a variable GF@input
+ */
+void cg_ifj_readi32(void);
+
+/**
+ * Reads a float from stdin and saves it into a variable GF@input
+ */
+void cg_ifj_readf64(void);
+
+/**
+ * Writes term to stdoiut
+ * \param term
+ */
 void cg_ifj_write(TTerm term);
 
 #endif
