f06b13cad8cb0aed3f00c335956e7147025dc1bf
Marek Pazúr <marekpazur1@seznam.cz>
2024-11-20 00:10:00 +0100

    Cleaned up redundant files

--- src/precedent.h
+++ src/precedent.h
@@ -8,10 +8,23 @@
 #ifndef PRECEDENT_H
 #define PRECEDENT_H
 
-#include "stack.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "symbol.h"
 #include "token.h"
 #include "lexer.h"
+
+/* ITEM STRUCTURE */
+typedef struct item {
+    symbol symbol;    // Symbol for precedence
+    struct item *next;
+} item_t;
+
+/* STACK STRUCTURE */
+typedef struct stack {
+    struct item *top;
+} stack_t;
 
 /* Enumeration of precedence table indexes */
 typedef enum pt_index_t {
@@ -25,6 +38,7 @@ typedef enum pt_index_t {
 	I_END_MARKER	// $ STACK BOTTOM
 } pt_index_t;
 
+// Precedence analysis functions
 /* Maps symbol into table */
 int pt_map(symbol term);
 
@@ -38,5 +52,37 @@ void equal(stack_t *stack, symbol next_symbol);			// = EQUAL
 
 /* Precedent analysis main function */
 int precedent(token_id end_marker);
+
+
+// Symbol stack functions
+/* Initializes stack */
+void init_stack(stack_t* stack);
+
+/* Allocates new item */
+struct item* alloc_item(symbol symbol);
+
+/* Pushes item on top of the stack */
+void push(struct stack *s, symbol symbol);
+
+/* Pops item out of top of the stack */
+symbol pop(struct stack *s);
+
+/* Returns top item without popping it from the stack */
+symbol get_top(struct stack *s);
+
+/* Returns TOPMOST terminal item */
+symbol get_topmost_term(struct stack *s);
+
+/* Returns the number of symbols to be reduced */
+int reduction_count(struct stack *s);
+
+/* Inserts shift symbol after topmost terminal */
+void insert_shift(struct stack *s);
+
+/* Frees stack content */
+void free_stack(struct stack *s);
+
+/* Prints the content of the stack, used for debugging */
+void print_stack_content(stack_t *stack);
 
 #endif
